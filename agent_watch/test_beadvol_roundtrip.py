9bfcca1401e8cbe97c15ad0ec4fc3511b55b1760
Anthony Edgar <you@example.com>
2025-06-14 15:35:23 -0700

    Auto-commit: updated test_beadvol_roundtrip.py

--- agent_watch/test_beadvol_roundtrip.py
+++ agent_watch/test_beadvol_roundtrip.py
@@ -1,3 +1,8 @@
+
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), 'beadcore'))
+
 from beadcore.bead import Bead
 from beadcore.beadvol_writer import BeadVolumeWriter
 from beadcore.beadvol_reader import BeadVolumeReader
